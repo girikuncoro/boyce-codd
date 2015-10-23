973369848c3f55f8a74d3b906f356042c39c00c6
girikuncoro <girikuncoro@gmail.com>
2015-10-23 01:08:34 -0400

    Fixed the size calculation bug by overriding compare

--- BCNF.java
+++ BCNF.java
@@ -2,158 +2,184 @@ import java.util.*;
 
 public class BCNF {
 
-  /**
-   * Implement your algorithm here
-   * @param attributeSet
-   * @param functionalDependencies
-   * @return set of attribute set
-   **/
-  public static Set<AttributeSet> decompose(AttributeSet attributeSet,
-                                            Set<FunctionalDependency> functionalDependencies) {
-	  Set<AttributeSet> result = new HashSet<AttributeSet>();
-	  
-	  // Pick subset set of attributes X from the relation, using powerset
-	  Set<AttributeSet> powerSet = getPowerset(attributeSet);
-	  
-	  for(AttributeSet x : powerSet) {
-		  // Find X+ (closure)
-		  AttributeSet xClosure = closure(x, functionalDependencies);
-		  
-		  // Consider the case where set of functional dependencies includes attributes not in the relation
-		  AttributeSet tmpClosure = new AttributeSet(); 
-		  
-		  for(Attribute attr : xClosure.getAttributes()) {
-			  if(attributeSet.contains(attr)) {
-				  tmpClosure.addAttribute(attr);
-			  }
-		  }
-		  xClosure = tmpClosure;
-		  
-		  // If X is super key or determines only itself, try different set of attributes
-		  if(xClosure.equals(x) || xClosure.equals(attributeSet)) {
-			  continue;
-		  }
-		  
-		  // Separate table into two tables: X+ and (X U (X+)^c)
-		  ArrayList<Attribute> xComplement = new ArrayList<Attribute>(x.getAttributes());
-		  
-		  for(Attribute attr : attributeSet.getAttributes()) {
-			  if(!xClosure.contains(attr)) {
-				  xComplement.add(attr);
-			  }
-		  }
-		  AttributeSet xClosureComplement = new AttributeSet(xComplement);
-		  
-		  // Separate two tables and recurse on each side
-		  result.addAll(decompose(xClosure, functionalDependencies));
-		  result.addAll(decompose(xClosureComplement, functionalDependencies));
+	/**
+	 * Implement your algorithm here
+	 * @param attributeSet
+	 * @param functionalDependencies
+	 * @return set of attribute set
+	 **/
+	public static Set<AttributeSet> decompose(AttributeSet attributeSet,
+			Set<FunctionalDependency> functionalDependencies) {
+		Set<AttributeSet> result = new HashSet<AttributeSet>();
 
-		  // Done if all attribute sets have been tried
-		  return result;
-	  }
-	  // In case no separation, return same attributeSet
-	  result.add(attributeSet);
-	  return result;
-  }
+		// Pick subset set of attributes X from the relation, using powerset
+		Set<AttributeSet> powerSet = getPowerset(attributeSet);
 
-  /**
-   * Recommended helper method
-   * @param attributeSet
-   * @param functionalDependencies
-   * @return newDependency (new attribute set of dependency)
-   **/
-  public static AttributeSet closure(AttributeSet attributeSet, Set<FunctionalDependency> functionalDependencies) {
-	  // Data structure to store count[w->z] := |w|
-	  HashMap<String, Integer> count = new HashMap<String, Integer>();
-	  
-	  // List[attr] = {w->z} for attr in w
-	  HashMap<String, Set<FunctionalDependency>> list = new HashMap<String, Set<FunctionalDependency>>();
-			  
-	  // Initialization
-	  for(FunctionalDependency fd : functionalDependencies) {
-		  count.put(fd.toString(), fd.getIndependent().size());
-		  Iterator<Attribute> indIterator = fd.getIndependent().iterator();
-		  
-		  while(indIterator.hasNext()) {
-			  Attribute indAttr = indIterator.next();
-			  
-			  // Init key
-			  if(!list.containsKey(indAttr.toString())) {
-				  Set<FunctionalDependency> fdSet = new HashSet<FunctionalDependency>();
-				  fdSet.add(fd);
-				  
-				  list.put(indAttr.toString(), fdSet);
-			  } else {
-				  list.get(indAttr.toString()).add(fd);
-			  }
-		  }
-	  }
-	  
-	  AttributeSet newDep = new AttributeSet(attributeSet);
-	  AttributeSet update = new AttributeSet(attributeSet);
-	  
-	  // Computation
-	  while(update.size() != 0) {
-		  Attribute attr = update.getAttributes().get(0);
-		  update.getAttributes().remove(0);
-		  
-		  if(list.containsKey(attr.toString())) {
-			  for(FunctionalDependency fd : list.get(attr.toString())) {
-				  Integer updatedCount = count.get(fd.toString()) - 1;
-				  count.put(fd.toString(), updatedCount);
-				  
-				  if(updatedCount == 0) {
-					  AttributeSet add = new AttributeSet();
-					  AttributeSet dep = fd.dependent();
-					  
-					  // add := Z - newDep
-					  for(Attribute a : dep.getAttributes()) {
-						  if(!newDep.contains(a)) {
-							  add.addAttribute(a);
-						  }
-					  }
-					  
-					  // newDep := newDep UNION add
-					  // update := update UNION add
-					  for(Attribute a : add.getAttributes()) {
-						  newDep.addAttribute(a);
-						  update.addAttribute(a);
-					  }
-				  }
-			  }
-		  }
-	  }
-	  
-	  return newDep;
-  }
-  
-  /**
-   * Helper function to get powerset of the given attributeSet
-   * e.g. powerset of {1,2,3} is {{},{2},{3},{2,3},{1,2},{1,3},{1,2,3},{1}}
-   * @param attributeSet
-   * @return powerSet
-   */
-  public static Set<AttributeSet> getPowerset(AttributeSet attributeSet) {
-	  Set<AttributeSet> sets = new HashSet<AttributeSet>();
-	  
-	  if(attributeSet.size() == 0) {
-		  sets.add(new AttributeSet(attributeSet));
-		  return sets;
-	  }
-	  
-	  List<Attribute> list = new ArrayList<Attribute>(attributeSet.getAttributes());
-	  Attribute head = list.get(0);
-	  AttributeSet rest = new AttributeSet(list.subList(1, list.size()));
-	  
-	  for(AttributeSet set : getPowerset(rest)) {
-		  AttributeSet newSet = new AttributeSet();
-		  newSet.addAttribute(head);
-		  newSet.addAllAttribute(set);
-		  sets.add(newSet);
-		  sets.add(set);
-	  }
+		for(AttributeSet x : powerSet) {
+			// Find X+ (closure)
+			AttributeSet xClosure = closure(x, functionalDependencies);
 
-	  return sets;
-  }
-  
+			// Consider the case where set of functional dependencies includes attributes not in the relation
+			AttributeSet tmpClosure = new AttributeSet(); 
+
+			for(Attribute attr : xClosure.getAttributes()) {
+				if(attributeSet.contains(attr)) {
+					tmpClosure.addAttribute(attr);
+				}
+			}
+			xClosure = tmpClosure;
+
+			// If X is super key or determines only itself, try different set of attributes
+			if(xClosure.equals(x) || xClosure.equals(attributeSet)) {
+				continue;
+			}
+
+			// Separate table into two tables: X+ and (X U (X+)^c)
+			ArrayList<Attribute> xComplement = new ArrayList<Attribute>(x.getAttributes());
+
+			for(Attribute attr : attributeSet.getAttributes()) {
+				if(!xClosure.contains(attr)) {
+					xComplement.add(attr);
+				}
+			}
+			AttributeSet xClosureComplement = new AttributeSet(xComplement);
+
+			// Separate two tables and recurse on each side
+			result.addAll(decompose(xClosure, functionalDependencies));
+			result.addAll(decompose(xClosureComplement, functionalDependencies));
+
+			// Done if all attribute sets have been tried
+			result = cleanSet(result);
+			return result;
+		}
+		// In case no separation, return same attributeSet
+		result.add(attributeSet);
+		result = cleanSet(result);
+		return result;
+	}
+
+	/**
+	 * Recommended helper method
+	 * @param attributeSet
+	 * @param functionalDependencies
+	 * @return newDependency (new attribute set of dependency)
+	 **/
+	public static AttributeSet closure(AttributeSet attributeSet, Set<FunctionalDependency> functionalDependencies) {
+		// Data structure to store count[w->z] := |w|
+		HashMap<String, Integer> count = new HashMap<String, Integer>();
+
+		// List[attr] = {w->z} for attr in w
+		HashMap<String, Set<FunctionalDependency>> list = new HashMap<String, Set<FunctionalDependency>>();
+
+		// Initialization
+		for(FunctionalDependency fd : functionalDependencies) {
+			count.put(fd.toString(), fd.getIndependent().size());
+			Iterator<Attribute> indIterator = fd.getIndependent().iterator();
+
+			while(indIterator.hasNext()) {
+				Attribute indAttr = indIterator.next();
+
+				// Init key
+				if(!list.containsKey(indAttr.toString())) {
+					Set<FunctionalDependency> fdSet = new HashSet<FunctionalDependency>();
+					fdSet.add(fd);
+
+					list.put(indAttr.toString(), fdSet);
+				} else {
+					list.get(indAttr.toString()).add(fd);
+				}
+			}
+		}
+
+		AttributeSet newDep = new AttributeSet(attributeSet);
+		AttributeSet update = new AttributeSet(attributeSet);
+
+		// Computation
+		while(update.size() != 0) {
+			Attribute attr = update.getAttributes().get(0);
+			update.getAttributes().remove(0);
+
+			if(list.containsKey(attr.toString())) {
+				for(FunctionalDependency fd : list.get(attr.toString())) {
+					Integer updatedCount = count.get(fd.toString()) - 1;
+					count.put(fd.toString(), updatedCount);
+
+					if(updatedCount == 0) {
+						AttributeSet add = new AttributeSet();
+						AttributeSet dep = fd.dependent();
+
+						// add := Z - newDep
+						for(Attribute a : dep.getAttributes()) {
+							if(!newDep.contains(a)) {
+								add.addAttribute(a);
+							}
+						}
+
+						// newDep := newDep UNION add
+						// update := update UNION add
+						for(Attribute a : add.getAttributes()) {
+							newDep.addAttribute(a);
+							update.addAttribute(a);
+						}
+					}
+				}
+			}
+		}
+
+		return newDep;
+	}
+
+	/**
+	 * Helper function to get powerset of the given attributeSet
+	 * e.g. powerset of {1,2,3} is {{},{2},{3},{2,3},{1,2},{1,3},{1,2,3},{1}}
+	 * @param attributeSet
+	 * @return powerSet
+	 */
+	public static Set<AttributeSet> getPowerset(AttributeSet attributeSet) {
+		// Compare must be overridden to avoid bug in the size calculation, using TreeSet instead of HashSet
+		Set<AttributeSet> sets = new TreeSet<AttributeSet> (new Comparator<AttributeSet>() {
+			public int compare(AttributeSet set1, AttributeSet set2) {
+				return set1.size() > set2.size() ? 1 : -1;
+			}
+		});
+
+		if(attributeSet.size() == 0) {
+			sets.add(new AttributeSet(attributeSet));
+			return sets;
+		}
+
+		List<Attribute> list = new ArrayList<Attribute>(attributeSet.getAttributes());
+		Attribute head = list.get(0);
+		AttributeSet rest = new AttributeSet(list.subList(1, list.size()));
+
+		for(AttributeSet set : getPowerset(rest)) {
+			AttributeSet newSet = new AttributeSet();
+			newSet.addAttribute(head);
+			newSet.addAllAttribute(set);
+			sets.add(newSet);
+			sets.add(set);
+		}
+
+		return sets;
+	}
+
+	/**
+	 * Helper function to clean empty set
+	 * @param newAttrs
+	 * @return cleanedAttrs
+	 */
+	public static Set<AttributeSet> cleanSet(Set<AttributeSet> newAttrs) {
+		Set<AttributeSet> cleanedAttrs = new HashSet<AttributeSet>();
+		for(AttributeSet attrs : newAttrs) {
+			AttributeSet tmpAttrs = new AttributeSet();
+			for(Attribute attr : attrs.getAttributes()) {
+				if(! attr.toString().isEmpty()) {
+					tmpAttrs.addAttribute(attr);
+				}
+			}
+			cleanedAttrs.add(tmpAttrs);
+		}
+		return cleanedAttrs;
+	}
+
 }
--- BCNFTest.java
+++ BCNFTest.java
@@ -18,7 +18,7 @@ public class BCNFTest {
     attrs.addAttribute(new Attribute("a"));
     attrs.addAttribute(new Attribute("b"));
     attrs.addAttribute(new Attribute("c"));
-    
+
     System.out.println(attrs);
 
     //create functional dependencies
@@ -42,4 +42,395 @@ public class BCNFTest {
     }
 
   }
+
+	@Test
+	public void test2BCNF() {
+		// construct table
+		AttributeSet attrs = new AttributeSet();
+		attrs.addAttribute(new Attribute("a"));
+		attrs.addAttribute(new Attribute("b"));
+		attrs.addAttribute(new Attribute("c"));
+		attrs.addAttribute(new Attribute("d"));
+		attrs.addAttribute(new Attribute("e"));
+
+		// create functional dependencies
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		AttributeSet ind = new AttributeSet();
+		AttributeSet dep = new AttributeSet();
+		ind.addAttribute(new Attribute("a"));
+		dep.addAttribute(new Attribute("b"));
+
+		FunctionalDependency fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("c"));
+		dep.addAttribute(new Attribute("d"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		// run client code
+		Set<AttributeSet> bcnf = BCNF.decompose(attrs, fds);
+
+		// verify output
+		System.out.println("test2");
+		assertEquals("Incorrect number of tables", 3, bcnf.size());
+		for (AttributeSet as : bcnf) {
+			System.out.println(as);
+		}
+
+	}
+
+	@Test
+	public void test3BCNF() {
+		// construct table
+		AttributeSet attrs = new AttributeSet();
+		attrs.addAttribute(new Attribute("a"));
+		attrs.addAttribute(new Attribute("b"));
+		attrs.addAttribute(new Attribute("f"));
+
+		// create functional dependencies
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		AttributeSet ind = new AttributeSet();
+		AttributeSet dep = new AttributeSet();
+		ind.addAttribute(new Attribute("a"));
+		ind.addAttribute(new Attribute("b"));
+		dep.addAttribute(new Attribute("f"));
+
+
+		FunctionalDependency fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("b"));
+		dep.addAttribute(new Attribute("f"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		// run client code
+		Set<AttributeSet> bcnf = BCNF.decompose(attrs, fds);
+
+		// verify output
+		System.out.println("test3");
+		assertEquals("Incorrect number of tables", 2, bcnf.size());
+		for (AttributeSet as : bcnf) {
+			System.out.println(as);
+		}
+
+	}
+
+	@Test
+	public void test4BCNF() {
+		// construct table
+		AttributeSet attrs = new AttributeSet();
+		attrs.addAttribute(new Attribute("a"));
+		attrs.addAttribute(new Attribute("b"));
+		attrs.addAttribute(new Attribute("c"));
+		attrs.addAttribute(new Attribute("d"));
+		attrs.addAttribute(new Attribute("e"));
+
+		// create functional dependencies
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		AttributeSet ind = new AttributeSet();
+		AttributeSet dep = new AttributeSet();
+		ind.addAttribute(new Attribute("d"));
+		dep.addAttribute(new Attribute("b"));
+
+		FunctionalDependency fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("c"));
+		ind.addAttribute(new Attribute("e"));
+		dep.addAttribute(new Attribute("a"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		// run client code
+		Set<AttributeSet> bcnf = BCNF.decompose(attrs, fds);
+
+		// verify output
+		assertEquals("Incorrect number of tables", 3, bcnf.size());
+		System.out.println("test4");
+		for (AttributeSet as : bcnf) {
+			System.out.println(as);
+		}
+		System.out.println("\n");
+
+	}
+
+//	@Test
+	public void test5BCNF() {
+		// construct table
+		AttributeSet attrs = new AttributeSet();
+		attrs.addAttribute(new Attribute("c"));
+		attrs.addAttribute(new Attribute("g"));
+		attrs.addAttribute(new Attribute("h"));
+		attrs.addAttribute(new Attribute("r"));
+		attrs.addAttribute(new Attribute("s"));
+		attrs.addAttribute(new Attribute("t"));
+
+		// create functional dependencies
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		AttributeSet ind = new AttributeSet();
+		AttributeSet dep = new AttributeSet();
+		ind.addAttribute(new Attribute("c"));
+		dep.addAttribute(new Attribute("t"));
+
+		FunctionalDependency fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("c"));
+		ind.addAttribute(new Attribute("s"));
+		dep.addAttribute(new Attribute("g"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("h"));
+		ind.addAttribute(new Attribute("r"));
+		dep.addAttribute(new Attribute("c"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("h"));
+		ind.addAttribute(new Attribute("s"));
+		dep.addAttribute(new Attribute("r"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("h"));
+		ind.addAttribute(new Attribute("t"));
+		dep.addAttribute(new Attribute("r"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		// run client code
+		Set<AttributeSet> bcnf = BCNF.decompose(attrs, fds);
+
+		// verify output
+		assertEquals("Incorrect number of tables", 4, bcnf.size());
+		System.out.println("test5");
+		for (AttributeSet as : bcnf) {
+			System.out.println(as);
+		}
+		System.out.println("\n");
+
+	}
+
+//	@Test
+	public void test6BCNF() {
+		// construct table
+		AttributeSet attrs = new AttributeSet();
+		attrs.addAttribute(new Attribute("a"));
+		attrs.addAttribute(new Attribute("c"));
+		attrs.addAttribute(new Attribute("d"));
+		attrs.addAttribute(new Attribute("g"));
+		attrs.addAttribute(new Attribute("h"));
+		attrs.addAttribute(new Attribute("l"));
+		attrs.addAttribute(new Attribute("n"));
+		attrs.addAttribute(new Attribute("p"));
+		attrs.addAttribute(new Attribute("r"));
+		attrs.addAttribute(new Attribute("s"));
+		attrs.addAttribute(new Attribute("t"));
+		attrs.addAttribute(new Attribute("u"));
+
+		// create functional dependencies
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		AttributeSet ind = new AttributeSet();
+		AttributeSet dep = new AttributeSet();
+		ind.addAttribute(new Attribute("c"));
+		dep.addAttribute(new Attribute("t"));
+		dep.addAttribute(new Attribute("u"));
+
+		FunctionalDependency fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("s"));
+		dep.addAttribute(new Attribute("n"));
+		dep.addAttribute(new Attribute("a"));
+		dep.addAttribute(new Attribute("p"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("t"));
+		dep.addAttribute(new Attribute("l"));
+		dep.addAttribute(new Attribute("d"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("c"));
+		ind.addAttribute(new Attribute("s"));
+		dep.addAttribute(new Attribute("g"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("c"));
+		ind.addAttribute(new Attribute("s"));
+		dep.addAttribute(new Attribute("g"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("h"));
+		ind.addAttribute(new Attribute("s"));
+		dep.addAttribute(new Attribute("r"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("h"));
+		ind.addAttribute(new Attribute("t"));
+		dep.addAttribute(new Attribute("r"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		// run client code
+		Set<AttributeSet> bcnf = BCNF.decompose(attrs, fds);
+
+		// verify output
+		assertEquals("Incorrect number of tables", 6, bcnf.size());
+		System.out.println("test6");
+		for (AttributeSet as : bcnf) {
+			System.out.println(as);
+		}
+		System.out.println("\n");
+
+	}
+
+	@Test
+	public void test7BCNF() {
+		// construct table
+		AttributeSet attrs = new AttributeSet();
+		attrs.addAttribute(new Attribute("a"));
+		attrs.addAttribute(new Attribute("b"));
+		attrs.addAttribute(new Attribute("c"));
+		attrs.addAttribute(new Attribute("d"));
+		attrs.addAttribute(new Attribute("e"));
+		attrs.addAttribute(new Attribute("g"));
+
+		// create functional dependencies
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		AttributeSet ind = new AttributeSet();
+		AttributeSet dep = new AttributeSet();
+		ind.addAttribute(new Attribute("c"));
+		ind.addAttribute(new Attribute("d"));
+		dep.addAttribute(new Attribute("b"));
+
+		FunctionalDependency fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("b"));
+		ind.addAttribute(new Attribute("e"));
+		dep.addAttribute(new Attribute("a"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+
+		// run client code
+		Set<AttributeSet> bcnf = BCNF.decompose(attrs, fds);
+
+		// verify output
+		assertEquals("Incorrect number of tables", 3, bcnf.size());
+		System.out.println("test7");
+		for (AttributeSet as : bcnf) {
+			System.out.println(as);
+		}
+		System.out.println("\n");
+
+	}
+
+	@Test
+	public void testComplexBCNF() {
+		// construct table
+		AttributeSet attrs = new AttributeSet();
+		attrs.addAttribute(new Attribute("a"));
+		attrs.addAttribute(new Attribute("b"));
+		attrs.addAttribute(new Attribute("c"));
+		attrs.addAttribute(new Attribute("d"));
+		attrs.addAttribute(new Attribute("e"));
+		attrs.addAttribute(new Attribute("f"));
+		attrs.addAttribute(new Attribute("g"));
+		attrs.addAttribute(new Attribute("h"));
+		attrs.addAttribute(new Attribute("i"));
+		attrs.addAttribute(new Attribute("j"));
+		attrs.addAttribute(new Attribute("k"));
+		attrs.addAttribute(new Attribute("l"));
+		attrs.addAttribute(new Attribute("m"));
+
+		// create functional dependencies
+		Set<FunctionalDependency> fds = new HashSet<FunctionalDependency>();
+		AttributeSet ind = new AttributeSet();
+		AttributeSet dep = new AttributeSet();
+		ind.addAttribute(new Attribute("a"));
+		dep.addAttribute(new Attribute("b"));
+		dep.addAttribute(new Attribute("c"));
+		dep.addAttribute(new Attribute("d"));
+		dep.addAttribute(new Attribute("e"));
+
+
+		FunctionalDependency fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("e"));
+		dep.addAttribute(new Attribute("f"));
+		dep.addAttribute(new Attribute("g"));
+		dep.addAttribute(new Attribute("h"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("i"));
+		dep.addAttribute(new Attribute("j"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("a"));
+		ind.addAttribute(new Attribute("i"));
+		dep.addAttribute(new Attribute("k"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		ind = new AttributeSet();
+		dep = new AttributeSet();
+		ind.addAttribute(new Attribute("a"));
+		ind.addAttribute(new Attribute("l"));
+		dep.addAttribute(new Attribute("m"));
+		fd = new FunctionalDependency(ind, dep);
+		fds.add(fd);
+
+		// run client code
+		Set<AttributeSet> bcnf = BCNF.decompose(attrs, fds);
+
+		// verify output
+		assertEquals("Incorrect number of tables", 6, bcnf.size());
+
+	}
 }
